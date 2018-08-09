33d4212ce4ddbe019577cdc1ee69fb0cc95bebb0
navamoore <navamoore@hotmail.com>
2018-08-09 15:18:50 -0400

    Initial commit

---
+++ RydbergEquation.py
@@ -0,0 +1,10 @@
+#Rydberg equation for find the wavelength emmission of a hydrogen atom.
+
+R = 1.097e-2
+for m in [1,2,3]:
+    print("Series for m =" ,m)
+    for k in [1,2,3,4,5]:
+        n = m + k
+        invlambda = R*(1/m**2-1/n**2)
+
+        print("  ", 1/invlambda, "nm")
